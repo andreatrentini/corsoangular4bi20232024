db5ca16a1955d5ae90d693c9aa7ed20a824cb588
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2024-02-21 09:52:26 +0000

    login form

--- spotify/src/app/app.routes.ts
+++ spotify/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { ArtistComponent } from './artist/artist.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'artists', component: ArtistComponent},
     {path: 'albums/:id', component: AlbumsComponent},
+    {path: 'login', component: LoginComponent},
     {path: '**', component: NotFoundComponent}
 ];
